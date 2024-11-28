b6d9c956d9cd61acfca3c747816b9e45f7ac92a1
cdtm0124 <c.mullins11@universityofgalway.ie>
2024-11-28 01:42:14 +0000

    Commented Signature.java

--- src/main/java/org/example/Signature.java
+++ src/main/java/org/example/Signature.java
@@ -1,19 +1,22 @@
 package org.example;
 
+// Represents a signature with a name and an email address
 public class Signature {
-    private String name;
-    private String email;
+    private String name; // Name of the person signing
+    private String email; // Email of the person signing
 
+    // Constructor to initialize a signature with name and email
     public Signature(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
-    // Getters
+    // Get the name of the signer
     public String getName() {
         return name;
     }
 
+    // Get the email of the signer
     public String getEmail() {
         return email;
     }
