35291491183920c8ded6cb0085cf4bd253c18a5e
cdtm0124 <c.mullins11@universityofgalway.ie>
2024-11-28 01:15:39 +0000

    Commented Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,18 @@
+// Defines the package for this class
 package org.example;
 
+// Required Spring Boot and REST components
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+// Marks this as the main Spring Boot application and REST controller
 @SpringBootApplication
 @RestController
 public class Main {
+
+	// Main method to start the application
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 }
-
