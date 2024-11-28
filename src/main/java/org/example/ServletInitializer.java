7f8c56fa068556e38cf28ec04e65314e5fb78325
cdtm0124 <c.mullins11@universityofgalway.ie>
2024-11-28 01:35:31 +0000

    Commented ServletInitializer.java

--- src/main/java/org/example/ServletInitializer.java
+++ src/main/java/org/example/ServletInitializer.java
@@ -3,10 +3,12 @@ package org.example;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// Configures the application to run in a servlet container (e.g., Tomcat)
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	// Overrides the configure method to link the Main class as the application's entry point
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Main.class);
+		return application.sources(Main.class); // Specifies the main application class
 	}
 }
