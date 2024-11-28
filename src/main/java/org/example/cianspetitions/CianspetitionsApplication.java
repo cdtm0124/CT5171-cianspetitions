70974a3f119a7fd0713c57c9f69d7b24e6451fd6
cdtm0124 <c.mullins11@universityofgalway.ie>
2024-11-28 00:22:11 +0000

    Initial Commit

---
+++ src/main/java/org/example/cianspetitions/CianspetitionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.cianspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CianspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CianspetitionsApplication.class, args);
+	}
+
+}
